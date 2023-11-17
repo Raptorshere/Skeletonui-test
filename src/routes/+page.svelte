5adee31e7f2de1e2cdd9fb31c23ed53e6b236c19
Kevin Fula <kevin.fula@edu.sait.ca>
2023-11-16 23:06:15 -0700

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="text-align-center container h-full mx-auto flex justify-center items-center space-y-4">
+	<div class="space-y-5">
+		<h1 class="h1">Seafoam</h1>
+		<p>Start by exploring:</p>
+		<div>
+			<ul>
+				<li>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
